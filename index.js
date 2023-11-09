ffbb6c12bfab89fcb34c3273533f490367c12af0
zaquari_andl <zaquariandl043@gmail.com>
2023-11-09 16:50:28 -0600

    fixed solutions for test 3 and 4

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ let multiply = num1 * num2
 
 let random = Math.round(Math.random() * 10) + 1;
 
-let num3 = 2
-let num4 = 8
-let mod = num4 / num3
+let num3 = 22
+let num4 =  9
+let mod = num3 % num4
 
-let max = Math.max(1, 20)
+let max = (1, 20)
